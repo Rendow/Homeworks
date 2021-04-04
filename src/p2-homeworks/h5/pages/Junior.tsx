9f5fba456887d4e41cd35f335b0812d15f169ad9
Rendow <rendow33@gmail.com>
2021-04-04 21:16:04 +0300

    commit

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HW7 from "../../h7/HW7";
 import Header from "../Header";
 import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 function Junior() {
     return (
@@ -9,10 +10,10 @@ function Junior() {
             <Header/>
             <HW7/>
             <HW8/>
-            {/* <HW9/>
-            <HW10/>
-            <HW11/>*/}
-
+             <HW9/>
+          {/*  <HW10/>
+            <HW11/>
+*/}
         </div>
     );
 }
